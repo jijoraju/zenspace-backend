af5b5ad35d56c080a8ee3fd152f7d49f446c5208
Jijo <jijoraju11@gmail.com>
2023-09-29 20:03:05 -0400

    Prima migrations

---
+++ prisma/migrations/20230930000049_dev/migration.sql
@@ -0,0 +1,179 @@
+-- CreateEnum
+CREATE TYPE "PaymentMethod" AS ENUM ('credit_card', 'PayPal');
+
+-- CreateEnum
+CREATE TYPE "NotificationStatus" AS ENUM ('read', 'unread');
+
+-- CreateTable
+CREATE TABLE "UserType" (
+    "type_id" SERIAL NOT NULL,
+    "type_description" TEXT NOT NULL,
+
+    CONSTRAINT "UserType_pkey" PRIMARY KEY ("type_id")
+);
+
+-- CreateTable
+CREATE TABLE "User" (
+    "user_id" SERIAL NOT NULL,
+    "first_name" TEXT NOT NULL,
+    "last_name" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "mobile" TEXT,
+    "profile_picture" TEXT,
+    "join_date" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "user_type_id" INTEGER NOT NULL,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("user_id")
+);
+
+-- CreateTable
+CREATE TABLE "Workspace" (
+    "workspace_id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+    "description" TEXT NOT NULL,
+    "address" TEXT NOT NULL,
+    "workspace_type" TEXT NOT NULL,
+    "price_per_hour" DOUBLE PRECISION NOT NULL,
+    "price_per_day" DOUBLE PRECISION NOT NULL,
+    "no_of_spaces" INTEGER NOT NULL,
+
+    CONSTRAINT "Workspace_pkey" PRIMARY KEY ("workspace_id")
+);
+
+-- CreateTable
+CREATE TABLE "WorkspacePhoto" (
+    "photo_id" SERIAL NOT NULL,
+    "photo_url" TEXT NOT NULL,
+    "workspace_id" INTEGER NOT NULL,
+
+    CONSTRAINT "WorkspacePhoto_pkey" PRIMARY KEY ("photo_id")
+);
+
+-- CreateTable
+CREATE TABLE "Review" (
+    "review_id" SERIAL NOT NULL,
+    "rating" INTEGER NOT NULL,
+    "comments" TEXT,
+    "review_date" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "workspace_id" INTEGER NOT NULL,
+
+    CONSTRAINT "Review_pkey" PRIMARY KEY ("review_id")
+);
+
+-- CreateTable
+CREATE TABLE "Payment" (
+    "payment_id" SERIAL NOT NULL,
+    "amount" DOUBLE PRECISION,
+    "payment_date" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "payment_method" "PaymentMethod",
+    "card_id" INTEGER NOT NULL,
+
+    CONSTRAINT "Payment_pkey" PRIMARY KEY ("payment_id")
+);
+
+-- CreateTable
+CREATE TABLE "Booking" (
+    "booking_id" SERIAL NOT NULL,
+    "start_date" TIMESTAMP(3) NOT NULL,
+    "end_date" TIMESTAMP(3) NOT NULL,
+    "booking_date" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "total_price" TEXT NOT NULL,
+    "workspace_id" INTEGER NOT NULL,
+    "user_id" INTEGER NOT NULL,
+    "payment_id" INTEGER NOT NULL,
+
+    CONSTRAINT "Booking_pkey" PRIMARY KEY ("booking_id")
+);
+
+-- CreateTable
+CREATE TABLE "Amenity" (
+    "amenity_id" SERIAL NOT NULL,
+    "description" TEXT,
+
+    CONSTRAINT "Amenity_pkey" PRIMARY KEY ("amenity_id")
+);
+
+-- CreateTable
+CREATE TABLE "Notification" (
+    "notification_id" SERIAL NOT NULL,
+    "message" TEXT NOT NULL,
+    "notification_date" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "status" "NotificationStatus" NOT NULL,
+    "booking_id" INTEGER NOT NULL,
+
+    CONSTRAINT "Notification_pkey" PRIMARY KEY ("notification_id")
+);
+
+-- CreateTable
+CREATE TABLE "Promotion" (
+    "promotion_id" SERIAL NOT NULL,
+    "promo_code" TEXT NOT NULL,
+    "description" TEXT NOT NULL,
+    "discount_percentage" INTEGER,
+    "discount_amount" INTEGER,
+    "start_date" TIMESTAMP(3),
+    "end_date" TIMESTAMP(3),
+
+    CONSTRAINT "Promotion_pkey" PRIMARY KEY ("promotion_id")
+);
+
+-- CreateTable
+CREATE TABLE "WorkspaceAmenities" (
+    "workspace_id" INTEGER NOT NULL,
+    "amenity_id" INTEGER NOT NULL,
+
+    CONSTRAINT "WorkspaceAmenities_pkey" PRIMARY KEY ("workspace_id","amenity_id")
+);
+
+-- CreateTable
+CREATE TABLE "UserPromotion" (
+    "user_promotion_id" SERIAL NOT NULL,
+    "used_date" TIMESTAMP(3) NOT NULL,
+    "user_id" INTEGER NOT NULL,
+    "promotion_id" INTEGER NOT NULL,
+
+    CONSTRAINT "UserPromotion_pkey" PRIMARY KEY ("user_promotion_id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_mobile_key" ON "User"("mobile");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Promotion_promo_code_key" ON "Promotion"("promo_code");
+
+-- AddForeignKey
+ALTER TABLE "User" ADD CONSTRAINT "User_user_type_id_fkey" FOREIGN KEY ("user_type_id") REFERENCES "UserType"("type_id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "WorkspacePhoto" ADD CONSTRAINT "WorkspacePhoto_workspace_id_fkey" FOREIGN KEY ("workspace_id") REFERENCES "Workspace"("workspace_id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Review" ADD CONSTRAINT "Review_workspace_id_fkey" FOREIGN KEY ("workspace_id") REFERENCES "Workspace"("workspace_id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Booking" ADD CONSTRAINT "Booking_workspace_id_fkey" FOREIGN KEY ("workspace_id") REFERENCES "Workspace"("workspace_id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Booking" ADD CONSTRAINT "Booking_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "User"("user_id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Booking" ADD CONSTRAINT "Booking_payment_id_fkey" FOREIGN KEY ("payment_id") REFERENCES "Payment"("payment_id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Notification" ADD CONSTRAINT "Notification_booking_id_fkey" FOREIGN KEY ("booking_id") REFERENCES "Booking"("booking_id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "WorkspaceAmenities" ADD CONSTRAINT "WorkspaceAmenities_workspace_id_fkey" FOREIGN KEY ("workspace_id") REFERENCES "Workspace"("workspace_id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "WorkspaceAmenities" ADD CONSTRAINT "WorkspaceAmenities_amenity_id_fkey" FOREIGN KEY ("amenity_id") REFERENCES "Amenity"("amenity_id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "UserPromotion" ADD CONSTRAINT "UserPromotion_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "User"("user_id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "UserPromotion" ADD CONSTRAINT "UserPromotion_promotion_id_fkey" FOREIGN KEY ("promotion_id") REFERENCES "Promotion"("promotion_id") ON DELETE RESTRICT ON UPDATE CASCADE;
