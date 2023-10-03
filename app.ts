ebc106d134e19b71d782fc3dfa61b85194f7f506
Jijo <jijoraju11@gmail.com>
2023-10-03 15:24:05 -0400

    cors settings added

--- app.ts
+++ app.ts
@@ -5,11 +5,28 @@ import logger from 'morgan';
 import { dirname } from 'path';
 import { fileURLToPath } from 'url';
 import swaggerUi from 'swagger-ui-express';
+import cors from 'cors';
 
 import passport from "./config/passport";
 import swaggerJSDoc from "swagger-jsdoc";
 import {options} from "./config/swagger";
 import router from "./routes";
+
+const allowedOrigins: string[] = ['http://localhost:5173', 'https://zenspace-backend.onrender.com'];
+
+const corsOptions: cors.CorsOptions = {
+    origin: function (origin: string | undefined, callback: (err: Error | null, allow?: boolean) => void) {
+        if (!origin || allowedOrigins.includes(origin)) {
+            callback(null, true);
+        } else {
+            callback(new Error('Not allowed by CORS'));
+        }
+    },
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+    optionsSuccessStatus: 204,
+    allowedHeaders: ['Content-Type', 'Authorization']
+};
 
 const __dirname: string = dirname(fileURLToPath(import.meta.url));
 const app: Express = express();
@@ -19,6 +36,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(cors(corsOptions));
 app.use(passport.initialize());
 
 app.use('/api', router);  // All routes will now be under /api
