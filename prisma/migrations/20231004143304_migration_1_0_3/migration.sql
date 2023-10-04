4c4eddb5dfc92e8fece4f1d3b0f91795bede8b4c
Jijo <jijoraju11@gmail.com>
2023-10-04 10:34:55 -0400

    data migration for location

---
+++ prisma/migrations/20231004143304_migration_1_0_3/migration.sql
@@ -0,0 +1,13 @@
+-- Insert data into the Location table
+INSERT INTO Location (name, province, latitude, longitude)
+VALUES
+    ('Toronto', 'Ontario', 43.653225, -79.383186),
+    ('Ottawa', 'Ontario', 45.421530, -75.690020),
+    ('Hamilton', 'Ontario', 43.255720, -79.871102),
+    ('London', 'Ontario', 42.984923, -81.245277),
+    ('Kitchener', 'Ontario', 43.450100, -80.482815),
+    ('Windsor', 'Ontario', 42.314937, -83.036363),
+    ('Mississauga', 'Ontario', 43.589046, -79.644120),
+    ('Brampton', 'Ontario', 43.683334, -79.766670),
+    ('Markham', 'Ontario', 43.856100, -79.337019),
+    ('Vaughan', 'Ontario', 43.797371, -79.552264);
